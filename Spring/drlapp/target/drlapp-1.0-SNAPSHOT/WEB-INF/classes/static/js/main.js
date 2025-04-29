400efcdce6ac00886ef079a64343ec0af0cb36d8
Cyter <tu2004latoi@gmail.com>
2025-04-29 09:56:16 +0700

    gopthumuc

---
+++ Spring/drlapp/target/drlapp-1.0-SNAPSHOT/WEB-INF/classes/static/js/main.js
@@ -0,0 +1,29 @@
+function deleteActivity(endpoint, id) {
+    // Kiểm tra nếu id không hợp lệ
+    if (!id) {
+        alert("ID không hợp lệ.");
+        return;
+    }
+
+    if (confirm("Bạn chắc chắn muốn xóa?") === true) {
+        fetch(`${endpoint}/${id}`, {
+            method: "DELETE"
+        })
+        .then(res => {
+            if (res.status === 204) {
+                alert("Xóa thành công");
+                location.reload();
+            } else {
+                // Xử lý chi tiết lỗi từ server
+                res.text().then(errorMessage => {
+                    alert("Hệ thống bị lỗi: " + errorMessage);
+                });
+            }
+        })
+        .catch(error => {
+            // Xử lý lỗi khi không thể thực hiện yêu cầu fetch
+            console.error("Có lỗi xảy ra khi xóa:", error);
+            alert("Có lỗi xảy ra khi xóa.");
+        });
+    }
+}
